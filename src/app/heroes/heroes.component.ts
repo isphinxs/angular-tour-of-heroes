cecf657f482382deb332f384768292f796fed627
Samantha Ostrowski <isphinxs@hotmail.com>
2022-01-29 14:59:57 -0500

    Create HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,12 +14,20 @@ export class HeroesComponent implements OnInit {
   //   name: 'Windstorm'
   // }
 
-  heroes = HEROES; 
+  // heroes = HEROES; 
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   
-  constructor() { }
+  constructor(
+    private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero): void {
