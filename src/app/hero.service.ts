8ab2084a27c09b30e2688ae548ae77e8264c5a38
Samantha Ostrowski <isphinxs@hotmail.com>
2022-02-06 14:48:59 -0500

    Update a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -73,6 +77,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     )
   }
 }
