d6b7d659391ea2a0a1e6e1d3a33d7c40eb47d0ff
Samantha Ostrowski <isphinxs@hotmail.com>
2022-02-06 14:56:42 -0500

    Add a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,7 +14,7 @@ export class HeroService {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
-  
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -47,6 +47,14 @@ export class HeroService {
 
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
 
   // getHeroes(): Hero[] {
